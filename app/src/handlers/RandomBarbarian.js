ef67594467b45f1b7d3de6a7491708734bb6520f
Sergio Rodriguez <oneandprime@gmail.com>
2017-09-29 21:48:27 -0400

    Slight tweakage

--- app/src/handlers/RandomBarbarian.js
+++ app/src/handlers/RandomBarbarian.js
@@ -3,7 +3,7 @@ const generators = require('random-rpg-stuff')
 module.exports = function () {
   const char = generators.characters.umdaar()
 
-  const msg = char.toString()
+  const msg = this.t('BARBARIAN_OATH') + ' ' + this.t('IT_IS') + '... ' + char.toString() + '!'
 
-  this.emit(':tellWithCard', msg, this.t('BARBARIAN_OATH'), msg)
+  this.emit(':tellWithCard', msg, this.t('SKILL_NAME'), msg)
 }
--- app/src/lang/en.js
+++ app/src/lang/en.js
@@ -1,10 +1,11 @@
 module.exports = {
   translation: {
-    SKILL_NAME: 'Random Gen',
+    SKILL_NAME: 'Random Barbarian',
     HELP_MESSAGE: 'Say "Hit Me" to get a random warrior of the blasted wasteland!  Or, you can say exit... Your choice.',
     HELP_REPROMPT: 'What can I help you with?',
     STOP_MESSAGE: 'Goodbye!',
-    RANDOM_BARBARIAN: 'Random Character',
-    BARBARIAN_OATH: 'By the Mane of Gorash'
+    RANDOM_BARBARIAN: 'Random Barbarian',
+    BARBARIAN_OATH: 'By the Mane of Gorash!',
+    IT_IS: 'It\'s'
   }
 }
